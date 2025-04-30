ac18015994ec3548fb4ebd7602189f2d2762070e
sberti <sberti@udesa.edu.ar>
2025-04-30 16:38:34 -0300

    Clase categorias y clase Ppal

--- src/Principal/Ppal.java
+++ src/Principal/Ppal.java
@@ -5,19 +5,13 @@ import java.util.*;
 public class Ppal {
 
 	public static void main(String[] args) {
-		List<Categoria> categorias = new ArrayList<>();
-        categorias.add(new Categoria("libros"));
-        categorias.add(new Categoria("alimentos"));
-        categorias.add(new Categoria("aalimasd"));
-        categorias.add(new Categoria("deportes"));
+		
 
-        Collections.sort(categorias); // ordena por nombre (alfabético)
+       // Collections.sort(categorias); //en un futuro usariamos esto para ordenar por nombre (alfabético)
         
        
 
-        for (Categoria c : categorias) {
-            System.out.println(c);
-        }
+        
 
 	}
 
